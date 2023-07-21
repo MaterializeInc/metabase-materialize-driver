3493e596efb1624377ca3865ac3897009b12e330
Bobby Iliev <bobby@bobbyiliev.com>
2023-07-21 13:19:19 +0300

    Remove custom JDBC driver (#19)
    * Remove the custom jdbc driver
    
    * Materialize specific connection properties
    
    * Update resources/metabase-plugin.yaml
    
    Co-authored-by: Joaquin Colacci <joaquincolacci@gmail.com>
    
    * Update resources/metabase-plugin.yaml
    
    Co-authored-by: Joaquin Colacci <joaquincolacci@gmail.com>
    
    ---------
    
    Co-authored-by: Joaquin Colacci <joaquincolacci@gmail.com>

--- src/metabase/driver/materialize.clj
+++ src/metabase/driver/materialize.clj
@@ -1,7 +1,7 @@
 (ns metabase.driver.materialize
   "Metabase Materialize Driver."
   (:require [clojure
-              [set :as set]]
+             [set :as set]]
             [metabase.db.spec :as db.spec]
             [metabase.driver :as driver]
             [metabase.driver.sql-jdbc
@@ -15,34 +15,16 @@
 ; ;;; |                                         metabase.driver.sql-jdbc impls                                         |
 ; ;;; +----------------------------------------------------------------------------------------------------------------+
 
-(defn- make-subname [host port db]
-  (str "//" host ":" port "/" db))
-
-(defn materialize
-  "Create a Clojure JDBC database specification for Materialize."
-  [{:keys [host port db]
-    :or   {host "localhost", port 6875, db "materialize"}
-    :as   opts}]
-  (merge
-   {:classname                     "io.materialize.Driver"
-    :subprotocol                   "materialize"
-    :subname                       (make-subname host port db)}
-   (dissoc opts :host :port :db)))
-
-(defmethod driver/display-name :materialize [_] "Materialize")
-
-(defmethod sql-jdbc.conn/connection-details->spec :materialize [_ {ssl? :ssl, :as details-map}]
-  (-> details-map
-      (update :port (fn [port]
-                      (if (string? port)
-                        (Integer/parseInt port)
-                        port)))
-      ;; remove :ssl in case it's false; DB will still try (& fail) to connect if the key is there
-      (dissoc :ssl)
-      (merge {:sslmode "disable"})
-      (set/rename-keys {:dbname :db})
-      materialize
-      (sql-jdbc.common/handle-additional-options details-map)))
+(defmethod sql-jdbc.conn/connection-details->spec :materialize
+  [_ {:keys [host port db], :as opts}]
+  (sql-jdbc.common/handle-additional-options
+   (merge
+    {:classname                     "org.postgresql.Driver"
+     :subprotocol                   "postgresql"
+     :subname                       (str "//" host ":" port "/" db)
+     :ssl                           true
+     :OpenSourceSubProtocolOverride false}
+    (dissoc opts :host :port :db))))
 
 (defmethod driver/describe-table :materialize
   [driver database table]
