840cba6ecb5dd36a5c6aa355669aeb3c636a3903
Bobby Iliev <bobby@bobbyiliev.com>
2025-01-24 19:34:35 +0200

    Remove [metabase.public-settings.premium-features :as premium-features] from the test namespace

--- test/metabase/driver/materialize_test.clj
+++ test/metabase/driver/materialize_test.clj
@@ -7,7 +7,6 @@
    [metabase.driver.materialize :as materialize]
    [metabase.driver.sql-jdbc.connection :as sql-jdbc.conn]
    [metabase.driver.sql.query-processor :as sql.qp]
-   [metabase.public-settings.premium-features :as premium-features]
    [metabase.query-processor :as qp]
    [metabase.test.fixtures :as fixtures]
    [metabase.test :as mt]))
